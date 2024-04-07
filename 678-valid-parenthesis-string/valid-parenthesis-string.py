911542e9cddfcd0b15a913654d1f7f42b70f0e80
Yaroslav Voropaev <128208855+zabojeb@users.noreply.github.com>
2024-04-07 18:22:56 +0300

    Time: 33 ms (74.42%) | Memory: 16.5 MB (50.32%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        mins, maxs = 0, 0
+        for c in s:
+            if c == "(":
+                mins += 1
+                maxs += 1
+            elif c == ")":
+                mins -= 1
+                maxs -=1
+            else:
+                mins -= 1
+                maxs +=1
+            
+            if maxs < 0:
+                return False
+            if mins < 0:
+                mins = 0
+                
+        return mins == 0
