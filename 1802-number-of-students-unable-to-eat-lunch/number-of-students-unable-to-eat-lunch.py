6ea93db2593d391af71ded8fbe919c91d1bf9240
Yaroslav Voropaev <128208855+zabojeb@users.noreply.github.com>
2024-04-08 15:34:16 +0300

    Time: 29 ms (98.57%) | Memory: 16.6 MB (59.90%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -1,14 +1,9 @@
-from collections import deque
-
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        students_queue = deque(students)
         for sandwich in sandwiches:
-            if sandwich in students_queue:
-                while students_queue[0] != sandwich:
-                    students_queue.rotate(-1)
-                students_queue.popleft()
+            if sandwich in students:
+                students.remove(sandwich)
             else:
-                return len(students_queue)
+                return len(students)
         
         return 0
