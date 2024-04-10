d82c9b7548e5790a20072fd932c5958f96184a8c
Yaroslav Voropaev <128208855+zabojeb@users.noreply.github.com>
2024-04-10 14:11:42 +0300

    Time: 57 ms (5.07%) | Memory: 16.7 MB (97.97%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
@@ -1,9 +1,10 @@
 class Solution:
-    def deckRevealedIncreasing(self, deck):
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
         deck.sort(reverse=True)
         result = [deck[0]]
 
         for card in deck[1:]:
-            result = [card] + [result[-1]] + result[:-1]
+            result.insert(0, result.pop())
+            result.insert(0, card)
 
         return result
