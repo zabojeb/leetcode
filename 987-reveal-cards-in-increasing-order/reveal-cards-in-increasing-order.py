23b41851b3431d675356f21efc22be02c999a5f6
Yaroslav Voropaev <128208855+zabojeb@users.noreply.github.com>
2024-04-10 14:06:09 +0300

    Time: 72 ms (5.07%) | Memory: 17 MB (9.42%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
@@ -1,15 +1,13 @@
+from queue import Queue
+
 class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
-        deck.sort()
-        n = len(deck)
-        ans = [0] * n
-        indices = list(range(n))
-        
-        i = 0
-        while indices:
-            ans[indices.pop(0)] = deck[i]
-            if indices:
-                indices.append(indices.pop(0))
-            i += 1
-        
-        return ans
+        q = Queue()
+
+        for card in reversed(sorted(deck)):
+            if not q.empty():
+                q.put(q.get())
+            q.put(card)
+
+        return reversed(list(q.queue))
+
