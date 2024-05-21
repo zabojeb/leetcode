06fc0c22965ec34d1538c1a916019bb8dc6a9e46
Yaroslav Voropaev <128208855+zabojeb@users.noreply.github.com>
2024-05-21 19:59:40 +0300

    Time: 39 ms (44.69%) | Memory: 16.5 MB (97.99%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,5 +1,3 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        masks = [1 << i for i in range(len(nums))]
-        for i in range(1 << len(nums)):
-            yield [ss for mask, ss in zip(masks, nums) if i & mask]
+        return chain.from_iterable(combinations(nums, r) for r in range(len(nums)+1))
