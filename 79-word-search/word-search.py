b8e1c926370d3dd49e748f00895a38a496f561cb
Yaroslav Voropaev <128208855+zabojeb@users.noreply.github.com>
2024-06-19 22:45:47 +0300

    Time: 3376 ms (79.39%) | Memory: 16.5 MB (54.64%) - LeetSync

---
+++ 79-word-search/word-search.py
@@ -0,0 +1,34 @@
+class Solution:
+    def exist(self, board, word):
+        def backtrack(i, j, k):
+            if k == len(word):
+                return True
+            if (
+                i < 0
+                or i >= len(board)
+                or j < 0
+                or j >= len(board[0])
+                or board[i][j] != word[k]
+            ):
+                return False
+
+            cache = board[i][j]
+            board[i][j] = ""
+
+            if (
+                backtrack(i + 1, j, k + 1)
+                or backtrack(i - 1, j, k + 1)
+                or backtrack(i, j + 1, k + 1)
+                or backtrack(i, j - 1, k + 1)
+            ):
+                return True
+
+            board[i][j] = cache
+            return False
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if backtrack(i, j, 0):
+                    return True
+
+        return False
